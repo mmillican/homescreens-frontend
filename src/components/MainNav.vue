9a3366c04b0b1d7ace8f7b2dfa6931e6a9ef6840
Matt Millican <matt@mattmillican.com>
2020-09-26 00:41:28 -0500

    Project setup + customizations

---
+++ src/components/MainNav.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <div class="container">
+      <b-navbar-brand to="/">Homescreens</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <!-- <template v-if="isAuthenticated"> -->
+            <b-nav-item to="/albums/mine">My Albums</b-nav-item>
+          <!-- </template> -->
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <!-- <b-navbar-nav class="ml-auto">
+          <b-nav-item to="/login" v-if="!isAuthenticated">Sign in</b-nav-item>
+          <b-nav-item-dropdown right v-if="isAuthenticated">
+            <template slot="button-content">{{ currentUser ? currentUser.firstName : '' }}</template>
+            <b-dropdown-item to="/user/profile">Profile</b-dropdown-item>
+            <b-dropdown-item href="#" v-on:click="logout">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav> -->
+      </b-collapse>
+    </div>
+  </b-navbar>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-Vue.config.productionTip = false
+import 'bootstrap/scss/bootstrap.scss';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(BootstrapVue);
+Vue.use(require('vue-moment'));
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
